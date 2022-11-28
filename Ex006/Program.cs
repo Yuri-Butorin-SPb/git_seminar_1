1b0bc5754dc0d274ecd13b59db3fb3d72060ff7c
Yuri <butor.yuri@gmail.com>
2022-11-28 17:20:20 +0300

    add Ex006 Bases

---
+++ Ex006/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 16;
+int b = 8;
+int c = 15;
+int d = 3;
+int e = 10;
+
+int max = a;
+
+if (max < b) max = b;
+if (max < c) max = c;
+if (max < d) max = d;
+if (max < e) max = e;
+
+Console.WriteLine(max);
+
+
